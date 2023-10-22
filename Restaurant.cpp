fb67f68a3087bb5b40e60db0758845bfa504df54
Tran-Trung-Nhut <ttnhut904@gmail.com>
2023-10-23 00:07:42 +0700

    FixBugversion

--- Restaurant.cpp
+++ Restaurant.cpp
@@ -42,7 +42,7 @@ class imp_res : public Restaurant
 						delete this->head;
 						this->head = nullptr;
 						this->tail = nullptr;
-						count -= 1;
+						this->count -= 1;
 						return;
 					}
 					customer* cus = this->head;
@@ -52,6 +52,20 @@ class imp_res : public Restaurant
 					delete cus;
 				}
 				
+				customer* getTable(){
+					if(this->count == 0) return nullptr;
+					customer* cus = this->head;
+					if(this->count == 1){
+						this->head = nullptr;
+						this->tail = nullptr;
+						this->count -= 1;
+						return cus;
+					}
+					this->head = this->head->next;
+					this->head->prev = nullptr;
+					this->count -= 1;
+					return cus;
+				}
 
 				customer* get(){
 					return this->head;
@@ -193,7 +207,8 @@ class imp_res : public Restaurant
 					}
 					return times;
 				}
-
+	
+	//Còn oán linh hay không nhỉ?
 	bool isOutofYin(){
 		customer* tmpCus = tail;
 		for(int i = 0; i < this->count;i++){
@@ -205,8 +220,20 @@ class imp_res : public Restaurant
 		return true;
 	}
 
-	//Trả về tên của oán linh tới đây gần nhất
-	string r_outYin(){
+	//Còn chú thuật sư không ta
+	bool isOutofYang(){
+		customer* tmpCus = tail;
+		for(int i = 0; i < this->count;i++){
+			if(tmpCus->energy > 0){
+				return false;
+			}
+			tmpCus = tmpCus->prev;
+		}
+		return true;
+	}
+
+	//Loại bỏ oán linh vô trễ nhất trả về tên của nó
+	string r_outYin_onlyOne(){
 		customer* tmpCus = tail;
 		string na;
 		for(int i = 0; i < this->count; i++){
@@ -231,20 +258,8 @@ class imp_res : public Restaurant
 		return "";
 	}
 
-	//Còn hay hết chú thuật sư
-	bool isOutofYang(){
-		customer* tmpCus = tail;
-		for(int i = 0; i < this->count;i++){
-			if(tmpCus->energy > 0){
-				return false;
-			}
-			tmpCus = tmpCus->prev;
-		}
-		return true;
-	}
-
-	//Trả về tên của chú thuật sư đến gần đây nhất
-	string r_outYang(){
+	//Loại bỏ thằng thuật sư vào trễ nhất trả về tên của nó
+	string r_outYang_onlyOne(){
 		customer* tmpCus = tail;
 		string na;
 		for(int i = 0; i < this->count; i++){
@@ -269,24 +284,83 @@ class imp_res : public Restaurant
 		return "";
 	}
 
-	void resetOrder(){
+	//Trả về tên của oán linh tới đây gần nhất
+	void r_outYin(){
+		customer* tmpCus = tail;
+		int tmp = this->count;
+		for(int i = 0; i < tmp || tmpCus != nullptr; i++){
+			if(tmpCus->energy < 0){
+				if(tmpCus == this->tail){
+					tmpCus->prev->next = nullptr;
+					this->tail = tmpCus->prev;
+				}else if(tmpCus == this->head){
+					tmpCus->next->prev = nullptr;
+					this->head = tmpCus->next;
+				}else{
+					tmpCus->prev->next = tmpCus->next;
+					tmpCus->next->prev = tmpCus->prev;
+				}
+				tmpCus->print();
+				customer* deleteNode = tmpCus;
+				tmpCus = tmpCus->prev;
+				delete deleteNode;
+				this->count--;
+			}else{
+			tmpCus = tmpCus->prev;
+			}
+		}
+	}
+
+	//Trả về tên của chú thuật sư đến gần đây nhất
+	void r_outYang(){
+		customer* tmpCus = tail;
+		int tmp = this->count;
+		for(int i = 0; i < tmp || tmpCus != nullptr; i++){
+			if(tmpCus->energy > 0){
+				if(tmpCus == this->tail){
+					tmpCus->prev->next = nullptr;
+					this->tail = tmpCus->prev;
+				}else if(tmpCus == this->head){
+					tmpCus->next->prev = nullptr;
+					this->head = tmpCus->next;
+				}else{
+					tmpCus->prev->next = tmpCus->next;
+					tmpCus->next->prev = tmpCus->prev;
+				}
+				tmpCus->print();
+				customer* deleteNode = tmpCus;
+				tmpCus = tmpCus->prev;
+				delete deleteNode;
+				this->count--;
+			}else{
+			tmpCus = tmpCus->prev;
+			}
+		}
+	}
+
+	void reset_without_print(){
 		for(int i = 0; i < this->count; i++){
-			customer* out_order = this->head;
-			this->head = this->head->next;
-			delete out_order;
+			customer* out_queue = this->tail;
+			this->tail = this->tail->prev;
+			delete out_queue;
 		}
 		this->head = nullptr;
 		this->tail = nullptr;
 		this->count = 0;
 	}
 
-	void print(){
-		customer* tmpQ = head;
-		for(int i = 0; i < this->count;i++){
-			cout << tmpQ->name <<"-"<< tmpQ->energy<<endl;
-			tmpQ = tmpQ->next;
+	void resetQueue_Order(){
+		for(int i = 0; i < this->count; i++){
+			this->tail->print();
+			customer* out_queue = this->tail;
+			this->tail = this->tail->prev;
+			delete out_queue;
 		}
+		this->head = nullptr;
+		this->tail = nullptr;
+		this->count = 0;
 	}
+
 	};
 	public:
 		queue* Queue = new queue();
@@ -380,13 +454,26 @@ class imp_res : public Restaurant
 				this->current = this->current->next;
 				delete out_res;
 			}
-			this->Order->resetOrder();
 			this->number_of_people = 0;
 			this->current = nullptr;
 		}
 
 		//Đuổi oán linh và trả vị trị X về người bên trái
 		void Yin_out(customer* out_res){
+			if(this->number_of_people == 1){
+				delete out_res;
+				this->current = nullptr;
+				this->number_of_people -= 1;
+				return;
+			}
+			if(this->number_of_people == 2){
+				this->current = out_res->prev;
+				this->current->next = this->current;
+				this->current->prev = this->current;
+				delete out_res;
+				this->number_of_people -= 1;
+				return;
+			}
 			this->current = out_res->prev;
 			out_res->prev->next = out_res->next;
 			out_res->next->prev = out_res->prev;
@@ -398,6 +485,20 @@ class imp_res : public Restaurant
 
 		//Đuổi chú thuật sư và trả vị trị X về người bên phải
 		void Yang_out(customer* out_res){
+			if(this->number_of_people == 1){
+				delete out_res;
+				this->current = nullptr;
+				this->number_of_people -= 1;
+				return;
+			}
+			if(this->number_of_people == 2){
+				this->current = out_res->next;
+				this->current->next = this->current;
+				this->current->prev = this->current;
+				delete out_res;
+				this->number_of_people -= 1;
+				return;
+			}
 			this->current = out_res->next;
 			out_res->prev->next = out_res->next;
 			out_res->next->prev = out_res->prev;
@@ -407,13 +508,62 @@ class imp_res : public Restaurant
 			this->number_of_people -= 1;
 		}
 
+		void RED_2(customer* cus){
+			if(this->number_of_people >= MAXSIZE/2){
+				customer* tmpCus = current;
+				int RES = INT16_MIN;
+				for(int i = 0;i < this->number_of_people;i++){
+					if(RES < abs(cus->energy - tmpCus->energy)){
+						this->current = tmpCus;
+						RES = abs(cus->energy - tmpCus->energy);
+					}
+					tmpCus = tmpCus->next;
+				}
+				
+				RES = cus->energy - this->current->energy;
+					
+				if(this->number_of_people == 0){		 
+					this->SitWherever(cus);
+				} else{
+					//Nếu chênh lệch sức mạnh nhiều nhất là số dương thì ngồi phải
+					if(RES > 0){ 
+						this->SitRight(cus);
+					}
+					//Ngược lại
+					else{								
+						this->SitLeft(cus);
+					}
+				};
+				return;
+			}
+			//Nếu chưa có khách nào thì khách đầu tiên muốn ngồi đâu cũng say yes	 
+			if(this->number_of_people == 0){		 
+				this->SitWherever(cus);
+			} else{
+				//Nếu energy của khách mới lớn hơn thì ngồi bên phải người vào trước đó
+				if(cus->energy >= this->current->energy){
+					this->SitRight(cus);
+				}
+				//Và ngược lại
+				else{								 
+					this->SitLeft(cus);
+				}
+			}
+		}
+
 		void RED(string name, int energy)
 		{
 			//Energy bằng 0 => cút
 			if(energy == 0) return;						
 
-			//Cùng tên => cút
-			customer* tmpCurrent = current;
+			customer *tmpQueue = this->Queue->get();
+			for(int i = 0;i < this->Queue->size(); i++){
+				if(tmpQueue->name == name) return;
+				tmpQueue = tmpQueue->next;
+			}
+
+			//Cùng tên trong bàn=> cút
+			customer* tmpCurrent = this->current;
 			for(int i = 0 ;i < number_of_people ;i++){
 				if(tmpCurrent->name == name) return;
 				tmpCurrent = tmpCurrent->next;
@@ -421,20 +571,22 @@ class imp_res : public Restaurant
 
 			//Vào quán thôi
 			customer *cus = new customer (name, energy, nullptr, nullptr);
-			
-			//Nếu MAXSIZE = 1 thì cho nó vô ngồi luôn và nếu đã có khách ngồi thì đưa vào hàng đợi luôn
-			if(MAXSIZE == 1){
-				if(this->number_of_people == 1){
-					this->Queue->enqueue(cus);
-				}else{
-					this->SitWherever(cus);
-				}
+
+			//Đủ người rồi đứng chờ đi
+			if(this->number_of_people == MAXSIZE){
+				this->Queue->enqueue(cus);
+				return;
+			}
+
+			//Nếu MAXSIZE = 1 và chưa có ai thì ngồi luôn đi má
+			if(MAXSIZE == 1 && this->number_of_people == 0){
+				this->SitWherever(cus);
 				return;
 			}
 
 			//Liệu số người vào được 1 nửa số lượng chỗ tối đa chưa
 			if(this->number_of_people >= MAXSIZE/2){
-				customer* tmpCus = this->current;
+				customer* tmpCus = current;
 				int RES = INT16_MIN;
 				for(int i = 0;i < this->number_of_people;i++){
 					if(RES < abs(energy - tmpCus->energy)){
@@ -445,23 +597,19 @@ class imp_res : public Restaurant
 				}
 				
 				RES = energy - this->current->energy;
-				if(this->number_of_people < MAXSIZE){	
-					if(this->number_of_people == 0){		 
-						this->SitWherever(cus);
-					} else{
-						//Nếu chênh lệch sức mạnh nhiều nhất là số dương thì ngồi phải
-						if(RES > 0){ 
-							this->SitRight(cus);
-						}
-						//Ngược lại
-						else{								
-							this->SitLeft(cus);
-						}
-					};
-				}else{
-					//Đưa khác vào hàng đợi nếu chỗ đã đủ											
-					this->Queue->enqueue(cus);
-				}
+					
+				if(this->number_of_people == 0){		 
+					this->SitWherever(cus);
+				} else{
+					//Nếu chênh lệch sức mạnh nhiều nhất là số dương thì ngồi phải
+					if(RES > 0){ 
+						this->SitRight(cus);
+					}
+					//Ngược lại
+					else{								
+						this->SitLeft(cus);
+					}
+				};
 				return;
 			}
 			//Nếu chưa có khách nào thì khách đầu tiên muốn ngồi đâu cũng say yes	 
@@ -484,10 +632,8 @@ class imp_res : public Restaurant
 		void Queue_to_Table(){
 			if(!this->Queue->empty() && this->number_of_people < MAXSIZE){
 				while(this->number_of_people < MAXSIZE && !this->Queue->empty()){
-					string na = this->Queue->get()->name;
-					int e = this->Queue->get()->energy;
-					this->RED(na,e);
-					this->Queue->dequeue();
+					customer* cus = this->Queue->getTable();
+					this->RED_2(cus);
 				}		
 			}
 			else{return;}			
@@ -496,6 +642,7 @@ class imp_res : public Restaurant
 		void BLUE(int num)
 		{
 			if(num >= this->number_of_people){
+				this->Order->reset_without_print();
 				this->resetRestaurant();
 			}else{
 				customer* out_res = this->current;
@@ -534,65 +681,85 @@ class imp_res : public Restaurant
 			customer* cusEnd_yin = this->current->next;
 			customer* cusStart_yang = this->current;
 			customer* cusEnd_yang = this->current->next;
+			int pass_yang = 2;
+			int pass_yin = 2;
 
 			while(cusStart_yin->energy > 0 || cusEnd_yin->energy > 0){
+				if(pass_yin > this->number_of_people && cusEnd_yin->energy > 0 && cusStart_yin->energy > 0) break;
 				if(cusStart_yin->energy > 0){
 					cusStart_yin = cusStart_yin->prev;
+					pass_yin += 1;
 				}
 				if(cusEnd_yin->energy > 0){
 					cusEnd_yin = cusEnd_yin->next;
+					pass_yin += 1;
 				}
 			}
 			
 			while(cusStart_yang->energy < 0 || cusEnd_yang->energy < 0){
+				if(pass_yang > this->number_of_people && cusEnd_yang->energy < 0 && cusStart_yang->energy < 0) break;
 				if(cusStart_yang->energy < 0){
 					cusStart_yang = cusStart_yang->prev;
+					pass_yang += 1;
 				}
 				if(cusEnd_yang->energy < 0){
 					cusEnd_yang = cusEnd_yang->next;
+					pass_yang += 1;
 				}
 			}
 
 			do{
 				if(cusStart_yin->energy < 0 && cusEnd_yin->energy < 0){
-					swapCustomer(cusStart_yin,cusEnd_yin);
+					if(pass_yin > this->number_of_people) break;
+ 					swapCustomer(cusStart_yin,cusEnd_yin);
 					customer* tmpCus = cusEnd_yin;
 					cusEnd_yin = cusStart_yin;
 					cusStart_yin = tmpCus;
 					if(cusStart_yin->prev == cusEnd_yin){
 						cusStart_yin = cusEnd_yin;
+						pass_yin += 1;
 					}else{
 						cusStart_yin = cusStart_yin->prev;
 						cusEnd_yin = cusEnd_yin->next;
+						pass_yin += 2;
 					}
 				}else{
+					if(pass_yin >= this->number_of_people && cusEnd_yin->energy > 0 && cusStart_yin->energy > 0) break;
 					if(cusStart_yin->energy > 0){
 						cusStart_yin = cusStart_yin->prev;
+						pass_yin += 1;
 					}
 					if(cusEnd_yin->energy > 0){
 						cusEnd_yin = cusEnd_yin->next;
+						pass_yin += 1;
 					}
 				}
 			}while(cusStart_yin != cusEnd_yin);
 
 			do{
 				if(cusStart_yang->energy > 0 && cusEnd_yang->energy > 0){
+					if(pass_yang > this->number_of_people)  break;
 					swapCustomer(cusEnd_yang,cusStart_yang);
 					customer* tmpCus = cusEnd_yang;
 					cusEnd_yang = cusStart_yang;
 					cusStart_yang = tmpCus;
 					if(cusStart_yang->prev == cusEnd_yang){
 						cusStart_yang = cusEnd_yang;
+						pass_yang += 1;
 					}else{
 						cusStart_yang = cusStart_yang->prev;
 						cusEnd_yang = cusEnd_yang->next;
+						pass_yang += 2;
 					}
 				}else{
+					if(pass_yang >= this->number_of_people && cusEnd_yang->energy < 0 && cusStart_yang->energy < 0) break;
 					if(cusStart_yang->energy < 0){
 						cusStart_yang = cusStart_yang->prev;
+						pass_yang += 1;
 					}
 					if(cusEnd_yang->energy < 0){
 						cusEnd_yang = cusEnd_yang->next;
+						pass_yang += 1;
 					}
 				}
 			}while(cusStart_yang != cusEnd_yang);
@@ -648,26 +815,34 @@ class imp_res : public Restaurant
 		}
 		void DOMAIN_EXPANSION()
 		{
-			if(this->number_of_people == 0 || this->number_of_people == 1){return;}
+			if(this->number_of_people == 0){return;}
 
-			int sum_yang = 0, sum_yin = 0;
+			int sum_yang = 0, sum = 0;
 			customer* tmpCus = this->current;
 			
-			//Tính tổng năng lượng của oán linh và chú thuật sư
+			//Tính tổng năng lượng của chú thuật sư và tổng năng lượng toàn bộ khách trong bàn
 			do{
 				if(tmpCus->energy > 0){
 					sum_yang += tmpCus->energy;
-				}else{
-					sum_yin += tmpCus->energy;
 				}
+				sum += tmpCus->energy;
 				tmpCus = tmpCus->next;
 			}while(tmpCus != this->current);
 
-			if(sum_yang == abs(sum_yin)){
-				this->resetRestaurant();
-			}else if(sum_yang > abs(sum_yin)){
+			//Tổng năng lượng
+			tmpCus = this->Queue->get();
+			for(int i = 0; i < this->Queue->size();i++){
+				if(tmpCus->energy >  0){
+					sum_yang += tmpCus->energy;
+				}
+				sum += tmpCus->energy;
+				tmpCus = tmpCus->next;
+			}
+
+			if(sum_yang >= abs(sum)){
+				this->Queue->r_outYin();
 				while(!this->Order->isOutofYin()){
-					string Yin_name = this->Order->r_outYin();
+					string Yin_name = this->Order->r_outYin_onlyOne();
 					customer* out_res = this->current;
 					
 					do{
@@ -680,9 +855,10 @@ class imp_res : public Restaurant
 						}
 					}while(out_res != this->current);
 				}
-			}else{
+			}else{			
+				this->Queue->r_outYang();
 				while(!this->Order->isOutofYang()){
-					string Yang_name = this->Order->r_outYang();
+					string Yang_name = this->Order->r_outYang_onlyOne();
 					customer* out_res = this->current;
 					
 					do{
@@ -701,7 +877,11 @@ class imp_res : public Restaurant
 		void LIGHT(int num)
 		{
 			if(num == 0){
-				this->Queue->print();
+				customer* tmpQueue = this->Queue->get();
+				for(int i = 0; i < this->Queue->size(); i++){
+					tmpQueue->print();
+					tmpQueue = tmpQueue->next;
+				}
 				return;
 			}
 			customer* tmpI = this->current;
@@ -710,12 +890,6 @@ class imp_res : public Restaurant
 					tmpI->print();
 					tmpI = tmpI->next;
 				}
-				// cout<<"---"<<endl;
-				// customer* tmpCus = this->Order->get();
-				// for(int i = 0;i < this->Order->size();i++){
-				// 	tmpCus->print();
-				// 	tmpCus = tmpCus->next;
-				// }
 			}
 			else{
 				for(int i = 0; i < this->number_of_people;i++){
